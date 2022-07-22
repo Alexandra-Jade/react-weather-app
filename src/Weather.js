e8fd17341ec8cce0215c1700cad08f421605915a
Alexandra-Jade <iamalexjade@gmail.com>
2022-07-22 15:18:14 +0200

    changed Hello to Hello Alex

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+ 
+
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(`The weather in ${props.city} is ${response.data.main.temp}℃`);
+    }
+
+    let apiKey = "476fc8273a4982024fc651e75dacfe77";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+
+    axios.get(apiUrl).then(handleResponse);
+    return <h2> Hello from Alex </h2>;
+}
