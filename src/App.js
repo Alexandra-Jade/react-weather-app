020154c87ef1b6f8f3ccacfdde2f9a9760f2be4c
Alexandra-Jade <iamalexjade@gmail.com>
2022-07-26 17:50:15 +0200

    Updated the api and formatted the date

--- src/App.js
+++ src/App.js
@@ -1,100 +1,16 @@
 import React from "react";
-
 import Weather from "./Weather";
 
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
 
-
-function App() {
-    return <div className = "App" >
-        <header className = "App-header" >
-      <div class="container">
-        <div class="weather-app-wrapper border border-white p-4 rounded-3">
-          <div class="weather-app">
-          <div className="navigationItems"><span class="me-2">Windhoek</span><span>| </span><span class="me-2">Gaborone</span><span>| </span><span class="me-2">Harare</span><span>| </span><span>Maputo</span> </div>
-            <form id="search-form" class="mb-3">
-              <div class="row">
-               <div></div>
-                <div class="col-9">
-                  <input
-                    type="search"
-                    placeholder="Type a city..."
-                    class="form-control"
-                    id="city-input"
-                    autocomplete="off"
-                  />
-                < Weather city="Cape Town"/>
-
-                </div>
-                <div class="col-3">
-                  <input
-                    type="submit"
-                    value="search"
-                    class="btn btn-primary w-100"
-                  />
-                </div>
-              </div>
-            </form>
-            <div class="overview">
-              <h1 id="city">Cape Town</h1>
-             
-              <ul>
-                <li>
-                  Last updated: Saturday June 11, 19:41 <span id="date"></span>
-                </li>
-                <li id="description">Clear</li>
-              </ul>
-            </div>
-            <div class="row">
-              <div class="col-6">
-                <div class="clearfix weather-temperature">
-                  <div class="d-flex flex-row bd-highlight mb-3">
-                    <img
-                      src="https://openweathermap.org/img/wn/10d@2x.png"
-                      alt="Rainy"
-                      id="icon"
-                    />
-                    <div class="p-2 bd-highlight">
-                      <strong id="temperature">12</strong>
-                      <div class="p-1 bd-highlight">
-                        <span class="units">°C</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-4 mb-3">
-                <div class="d-flex flex-row-reverse bd-highlight">
-                  <ul>
-                    <li>
-                      Humidity: <span id="humidity">73 </span>%
-                    </li>
-                    <li>
-                      Wind: <span id="wind">2</span>km/h
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div class="container">
-        <div class="col">12° 11° 10° 10° 12°</div>
-      </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <small class="github-link">
-        <a
-          href="https://github.com/Alexandra-Jade/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a> 
-        by Alexandra Jade Rodrigues
-      </small>
-        </header> 
-        </div> ;
+export default function App() {
+    return <div className = "App" > 
+    <div className="container">
+    <Weather defaultCity="New York"/>
+    <footer> This project was coded by Alexandra Jade Rodrigues and is {""}
+       <a href="https://github.com/Alexandra-Jade/react-weather-app"
+       target="_blank"
+       rel="noreferrer">open sourced on GitHub</a> </footer>
+       </div>
+       </div>
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,84 @@
-import React from "react";
+import React, {useState} from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
+import "./Weather.css";
 
-
- 
 
 export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState ({ ready:false });
+
     function handleResponse(response) {
-        alert(`The weather in ${props.city} is ${response.data.main.temp}℃`);
+        setWeatherData({
+            ready:true,
+            temperature:response.data.main.temp,
+            date: new Date(response.data.dt * 1000),
+            description:response.data.weather[0].description,
+            iconUrl:"http://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            humidity:response.data.main.humidity,
+            wind:response.data.wind.speed,
+            city: response.data.name,
+         
+        });
     }
 
-    let apiKey = "476fc8273a4982024fc651e75dacfe77";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+if (weatherData.ready) {
+    return (<div className="Weather">
+    <form> 
+        <div className="row">
+        <div class="col-9"> 
+        <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                id="city-input"
+                autocomplete="on"
+              />
+              </div>
+              <div class="col-3"> 
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100"
+              />
+              </div>
+              </div>
+              </form>
+    <h1>{weatherData.city}</h1>
+    <ul>
+    <li> <FormattedDate date={weatherData.date} /> </li>
+    <li className="text-capitalize">{weatherData.description}</li>
+    </ul>
+   <div className="row mt-3">
+    <div className="col-6">
+    <div className="clearfix">
+    <img src={weatherData.iconUrl} alt={weatherData.description} className="float-left"/>
+   <div className="float-left">
+   <span className="temperature">{Math.round(weatherData.temperature)}</span><span className="units">°C</span>
+   </div>
+   </div>
+   </div>
+<div className="col-6">
+<ul>
+    <li>Precipitation: 15%</li>
+    <li>Humidity: {weatherData.humidity}%</li>
+    <li>Wind: {weatherData.wind} km/h</li>
+</ul>
+</div>
+</div>     
+</div> ); 
 
-
+} else {
+    const apiKey = "476fc8273a4982024fc651e75dacfe77";
+    let city ="New York"
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-    return <h2>  </h2>;
+
+    return "loading..."
+
+}
+
+   
+
+
+   
 }
