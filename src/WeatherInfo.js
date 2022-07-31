40a795a8e912f7b5c2a28eb0bd22f5e7ec5ffa0c
Alexandra-Jade <iamalexjade@gmail.com>
2022-07-31 09:52:35 +0200

    Updated Temperature Functions

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return ( 
@@ -14,7 +15,8 @@ export default function WeatherInfo(props) {
     <div className="clearfix">
     <img src={props.data.iconUrl} alt={props.data.description} className="float-left"/>
    <div className="float-left">
-   <span className="temperature">{Math.round(props.data.temperature)}</span><span className="units">°C</span>
+    <WeatherTemperature celsius={props.data.temperature}/>
+   
    </div>
    </div>
    </div>
